16da930d381176654365b4cd371ca8013ed89b39
Boris <boris1488ss@gmail.com>
2021-05-28 14:47:34 +0300

    10-13 modals

--- custom.js
+++ custom.js
@@ -1,6 +1,15 @@
 $( document ).ready(function() {
-  $('.ap-modal-window__close').click(function () {
+  $('.js-modal-close').click(function () {
     $(this).closest('.ap-modal-window').remove(); // to remove it from DOM
     // $(this).closest('.ap-modal-window').hide(); // to reuse it
+  });
+  $('.js-modal-close-w').click(function () {
+    $(this).closest('.ap-modal-window-wrapper').remove(); // to remove it from DOM
+    // $(this).closest('.ap-modal-window-wrapper').hide(); // to reuse it
+  });
+  $(document).click(function (e) {
+    if (e.target === $('.ap-modal-window-wrapper')[0]){
+      $('.ap-modal-window-wrapper').remove();
+    }
   });
 });
